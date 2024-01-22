23b97fd8c47c8b04dc3cb32b4f0cc3546fb640f8
Laiba Tariq <laiba.tariq@devsinc.com>
2024-01-22 14:11:36 +0500

    *seed Data Added seed data to Folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,25 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 user1 = User.create({ email: 'saad12@gmail.com', password: 'saad1234', username: 'saad ali',
-              usertype: 'manager' })
+                      usertype: 'manager' })
 user2 = User.create({ email: 'aliza23@gmail.com', password: 'aliza23', username: 'Aliza Tariq',
-              usertype: 'developer' })
+                      usertype: 'developer' })
 user3 = User.create({ email: 'saliha90@gmail.com', password: 'saliha23', username: 'Saliha Qaiser',
-              usertype: 'developer' })
+                      usertype: 'developer' })
 User.create({ email: 'abdullah87@gmail.com', password: 'abdullah89', username: 'Abdullah Nasir',
               usertype: 'QA' })
 
-project1 = Project.create({project_name: "project1", project_description: "Project1 Description", created_by: user1.id})
-project2 = Project.create({project_name: "project2", project_description: "Project2 Description", created_by: user1.id})
-project3 = Project.create({project_name: "project3", project_description: "Project3 Description", created_by: user2.id})
-project4 = Project.create({project_name: "project4", project_description: "Project4 Description", created_by: user2.id})
-project5 = Project.create({project_name: "project5", project_description: "Project5 Description", created_by: user2.id})
-project6 = Project.create({project_name: "project6", project_description: "Project6 Description", created_by: user2.id})
-project7 = Project.create({project_name: "project7", project_description: "Project7 Description", created_by: user3.id})
-
-bug1 = Bu
+project1 = Project.create({ project_name: 'project1', project_description: 'Project1 Description',
+                            created_by: user1.id })
+project2 = Project.create({ project_name: 'project2', project_description: 'Project2 Description',
+                            created_by: user1.id })
+project3 = Project.create({ project_name: 'project3', project_description: 'Project3 Description',
+                            created_by: user2.id })
+project4 = Project.create({ project_name: 'project4', project_description: 'Project4 Description',
+                            created_by: user2.id })
+project5 = Project.create({ project_name: 'project5', project_description: 'Project5 Description',
+                            created_by: user2.id })
+project6 = Project.create({ project_name: 'project6', project_description: 'Project6 Description',
+                            created_by: user2.id })
+project7 = Project.create({ project_name: 'project7', project_description: 'Project7 Description',
+                            created_by: user3.id })
