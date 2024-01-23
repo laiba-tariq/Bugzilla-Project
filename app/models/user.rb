1c8cf3051e04458945cb7564d4b17c866b301316
Laiba Tariq <laiba.tariq@devsinc.com>
2024-01-23 17:04:19 +0500

    updated model validations (user and user_projects)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord # rubocop:disable Style/Documentation
+  enum role: { manager: 0, qa: 1, developer: 2 }
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :user_projects
   has_many :projects, through: :user_projects
   has_many :bugs
-  enum role: { manager: 0, qa: 1, developer: 2 }
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
   validates :role, presence: true
 end
--- app/models/user_project.rb
+++ app/models/user_project.rb
@@ -3,4 +3,5 @@
 class UserProject < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  validates :user_id, uniqueness: { scope: :project_id }
 end
